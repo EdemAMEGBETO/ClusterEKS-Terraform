537b2e94aaa408e709de558e8b09a185d7b77f04
Edem AMEGBETO <eamegbeto@alphosa.com>
2026-05-06 13:49:28 +0200

    Mise à jour conf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.3"
+  required_version = "~> 1.13.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "dev-edem-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile   = true
     encrypt        = true
   }
 }
